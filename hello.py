9915471f499d120a72a38ea8d234af215b796aff
karthikeyankkn7 <152183950+karthikeyankkn7@users.noreply.github.com>
2023-12-02 18:34:11 +0000

    Update hello.py
    wsgi

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from gevent.pywsgi import WSGIServer
 app = Flask(__name__)
 @app.route("/")#URL leading to method
 def hello(): # Name of the method
  return("Hello World!") #indent this line
  
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port='8080') # indent this line
+ http_server = WSGIServer(('', 8080), app)
+ http_server.serve_forever()
+ #app.run(host='0.0.0.0', port='8080') # indent this line
  
