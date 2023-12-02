340cd968ca591b2f7152411bc31db71f4759494e
karthikeyankkn7 <152183950+karthikeyankkn7@users.noreply.github.com>
2023-12-02 18:31:55 +0000

    Update hello.py
    chn

--- hello.py
+++ hello.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 @app.route("/")#URL leading to method
 def hello(): # Name of the method
  return("Hello World!") #indent this line
+ 
 if __name__ == "__main__":
- from waitress import serve
- serve(app, host="0.0.0.0", port=8080)
-  #app.run(host='0.0.0.0', port='8080') # indent this line
+  app.run(host='0.0.0.0', port='8080') # indent this line
  
