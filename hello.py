4902ebbb9860d6e51ed59b94de6570c9aae0e5c5
karthikeyankkn7 <152183950+karthikeyankkn7@users.noreply.github.com>
2023-12-02 18:29:34 +0000

    Update hello.py
    changed2

--- hello.py
+++ hello.py
@@ -5,6 +5,6 @@ def hello(): # Name of the method
  return("Hello World!") #indent this line
 if __name__ == "__main__":
  from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+ serve(app, host="0.0.0.0", port=8080)
   #app.run(host='0.0.0.0', port='8080') # indent this line
  
